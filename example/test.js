4436863d9605a8940114b0e0219275edf758874b
luiscruzga <luis.cruzga@gmail.com>
2020-12-28 12:55:57 -0300

    Modify test

--- example/test.js
+++ example/test.js
@@ -1,5 +1,5 @@
 const toonify = require('../src/');
-const cartoon = new toonify('7dc8f27d-1aae-4b13-92e3-76d1abde2529');
+const cartoon = new toonify('YOU_API_KEY_HERE');
 
 cartoon.transform({
     photo: 'https://media.gq.com.mx/photos/5e220ec2ffa8c7000803441e/16:9/w_1920,c_limit/40-datos-curiosos-para-descubrir-a-scarlett-johansson.jpg',
